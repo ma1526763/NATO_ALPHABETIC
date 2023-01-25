17e312350dd8c2801f93843d67a4d46bbe8acd1c
aliriax <ma1526763@gmail.com>
2023-01-26 03:57:40 +0500

    validation

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pandas
+
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row["letter"]: row.code for index, row in data.iterrows()}
-name = input("Enter name: ").upper()
-nato_alpha = [data_dict[letter] for letter in name if letter in data_dict.keys()]
+while True:
+    name = input("Enter name: ")
+    name = name.upper().strip()
+    for letter in name:
+        if letter in data_dict.keys() or letter == " ":
+            pass
+        else:
+            name = input("Please enter letters in alphabet only.\nEnter name: ")
+            continue
+    break
+
+nato_alpha = []
+for letter in name:
+    if letter in data_dict.keys() and data_dict[letter] not in nato_alpha:
+        nato_alpha.append(data_dict[letter])
 print(nato_alpha)
